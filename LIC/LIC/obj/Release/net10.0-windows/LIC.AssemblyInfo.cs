6e2e1ccf34ddc6862b6f7a19f5e0a3fe042375e9
Nguyen Dinh Thi <dinhthi112@gmail.com>
2026-03-10 08:15:13 +0700

    them chuong trinh

--- LIC/LIC/obj/Release/net10.0-windows/LIC.AssemblyInfo.cs
+++ LIC/LIC/obj/Release/net10.0-windows/LIC.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LIC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d0c58f7baecca743a8055936923bfd735e2a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LIC")]
